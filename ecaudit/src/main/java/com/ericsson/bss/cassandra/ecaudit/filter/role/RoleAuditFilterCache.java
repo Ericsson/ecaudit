8557ec90454ccca10c11940c70e65918892cbe4a
Ted Petersson <ted.petersson@ericsson.com>
2020-03-11 10:56:26 +0100

    Configurable cache for role based whitelist filter (#151)
    Replace the AuditWhitelistCache to hava a "top-level" cache for
    RoleAuditFilter. This cache will also cover the authorize-call which may
    be expensive.
    
    Since the cache inherits from AuthCache its configuration (Validity/UpdateInterval/MaxEntries)
    is updateable via the cache MBean.
    
    Use EqualsVerifier to test all equals/hashCode implementations

---
+++ ecaudit/src/main/java/com/ericsson/bss/cassandra/ecaudit/filter/role/RoleAuditFilterCache.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2020 Telefonaktiebolaget LM Ericsson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ericsson.bss.cassandra.ecaudit.filter.role;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.util.concurrent.UncheckedExecutionException;
+
+import com.ericsson.bss.cassandra.ecaudit.auth.cache.AuthCache;
+import com.ericsson.bss.cassandra.ecaudit.config.AuditConfig;
+
+public class RoleAuditFilterCache extends AuthCache<RoleAuditFilterCacheKey, Boolean>
+{
+    private static final AtomicInteger UNIQUE_ID = new AtomicInteger();
+
+    RoleAuditFilterCache(Function<RoleAuditFilterCacheKey, Boolean> loadFunction)
+    {
+        this(loadFunction, AuditConfig.getInstance());
+    }
+
+    @VisibleForTesting
+    RoleAuditFilterCache(Function<RoleAuditFilterCacheKey, Boolean> loadFunction, AuditConfig auditConfig)
+    {
+        super("RoleAuditFilterCache" + UNIQUE_ID.incrementAndGet(), // Unique name is needed for unit tests to work
+              auditConfig::setWhitelistCacheValidity,
+              auditConfig::getWhitelistCacheValidity,
+              auditConfig::setWhitelistCacheUpdateInterval,
+              auditConfig::getWhitelistCacheUpdateInterval,
+              auditConfig::setWhitelistCacheMaxEntries,
+              auditConfig::getWhitelistCacheMaxEntries,
+              loadFunction,
+              () -> true);
+    }
+
+    public boolean isWhitelisted(RoleAuditFilterCacheKey cacheKey)
+    {
+        try
+        {
+            return get(cacheKey);
+        }
+        catch (ExecutionException e)
+        {
+            throw new UncheckedExecutionException(e);
+        }
+    }
+}
