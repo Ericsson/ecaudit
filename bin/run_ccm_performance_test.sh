5d9b26d5de363673ab1684bf5f9c10c477fdc569
Per Otterström <per.otterstrom@ericsson.com>
2020-04-03 11:01:08 +0200

    Make performance graphs more consistent

--- bin/run_ccm_performance_test.sh
+++ bin/run_ccm_performance_test.sh
@@ -50,6 +50,19 @@ if [ ! -f ${JAR_FILE} ]; then
  exit 3
 fi
 
+disable_compaction() {
+  ccm node1 cqlsh -u cassandra -p cassandra -x "CREATE KEYSPACE keyspace1 WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};"
+  ccm node1 cqlsh -u cassandra -p cassandra -x "CREATE TABLE keyspace1.standard1 (
+    key blob PRIMARY KEY,
+    \"C0\" blob,
+    \"C1\" blob,
+    \"C2\" blob,
+    \"C3\" blob,
+    \"C4\" blob
+);"
+  ccm node1 nodetool disableautocompaction
+}
+
 create_dummy_tables() {
   tmpfile=$(mktemp /tmp/perf_test_data_XXXX.cql)
   echo "CREATE KEYSPACE aperfks WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};" >> $tmpfile
@@ -86,18 +99,30 @@ set_yaml_based_filter() {
   done
 }
 
+tune_cassandra() {
+  CCM_CLUSTER_NAME=`cat ${CCM_CONFIG}/CURRENT`
+  CLUSTER_PATH=${CCM_CONFIG}/${CCM_CLUSTER_NAME}
+  echo "JVM_EXTRA_OPTS=\"\$JVM_EXTRA_OPTS -Xms16G -Xmx16G -Xmn4G"\" >> ${CLUSTER_PATH}/cassandra.in.sh
+  #echo "JVM_EXTRA_OPTS=\"\$JVM_EXTRA_OPTS -XX:+UnlockDiagnosticVMOptions -XX:ParGCCardsPerStrideChunk=4096"\" >> ${CLUSTER_PATH}/cassandra.in.sh
+  echo "disk_access_mode: mmap_index_only" >> ${CLUSTER_PATH}/node1/conf/cassandra.yaml
+  sed -i 's/^memtable_allocation_type.*/memtable_allocation_type: offheap_objects/' ${CLUSTER_PATH}/node1/conf/cassandra.yaml
+  sed -i 's/^-XX:SurvivorRatio=.*/-XX:SurvivorRatio=2/' ${CLUSTER_PATH}/node1/conf/cassandra.yaml
+}
+
 create_cluster() {
   ccm create -n 1 -v ${CLUSTER_VERSION} ecaudit-${1}
   if [[ $? -ne 0 ]]; then
    echo "Failed to create ccm cluster 'ecaudit'"
    exit 3
   fi
+
+  tune_cassandra
 }
 
 start_cassandra() {
   ccm start
   sleep 30
-  ccm node1 nodetool disableautocompaction
+  disable_compaction
   create_dummy_tables
 }
 
@@ -107,7 +132,7 @@ stop_cassandra() {
 }
 
 run_stress() {
-  ${CCM_CONFIG}/repository/3.11.4/tools/bin/cassandra-stress write n=3000000 -node 127.0.0.1 -port jmx=7100 -mode native cql3 user=cassandra password=cassandra -rate threads=10 -graph file=ecaudit-performance.html title=ecAudit-Performance revision=${1}
+  ${CCM_CONFIG}/repository/3.11.4/tools/bin/cassandra-stress write n=5000000 -node 127.0.0.1 -port jmx=7100 -mode native cql3 user=cassandra password=cassandra -rate threads=10 -graph file=ecaudit-performance.html title=ecAudit-Performance revision=${1}
 }
 
 echo "Generating performance report into ecaudit-performance.html"
