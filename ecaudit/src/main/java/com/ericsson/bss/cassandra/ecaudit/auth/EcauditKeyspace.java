bcfa248ff583365504f18fcd3dbbc13eb0c1e78d
tommy stendahl <tommy.stendahl@ericsson.com>
2025-04-25 15:47:43 +0200

    Fix comment

--- ecaudit/src/main/java/com/ericsson/bss/cassandra/ecaudit/auth/EcauditKeyspace.java
+++ ecaudit/src/main/java/com/ericsson/bss/cassandra/ecaudit/auth/EcauditKeyspace.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Telefonaktiebolaget LM Ericsson
+ * Copyright 2025 Telefonaktiebolaget LM Ericsson
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
